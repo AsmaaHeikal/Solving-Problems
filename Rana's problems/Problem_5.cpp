ff7a6db4a8787346ac05dbda7fad92bdb7755530
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2022-10-14 23:13:43 +0200

    problem_5.cpp update

--- Rana's problems/Problem_5.cpp
+++ Rana's problems/Problem_5.cpp
@@ -2,87 +2,46 @@
 // purpose : Domino chain checker
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct dominoT{
-    int leftDots ;
-    int rightDots;
-    dominoT(){}
-    dominoT(int leftDots, int rightDots){
-        this-> leftDots = leftDots;
-        this -> rightDots = rightDots;
-    }
+struct domino{
+    int left, right;
 };
-bool canAppend(dominoT currDominoes, dominoT leftDomino , dominoT rightDomino);
-bool FormsDominoChain(vector<dominoT> & dominoes);
+
+vector<domino> chain;
+map<int, bool>chck;
+
+bool FormChain(vector <domino> dmns){
+    //base case
+    if(chain.size() == dmns.size()) return true;
+    for(int i = 0 ; i < dmns.size() ; i++){
+        if(chain.empty() || ((chain[chain.size()-1].right== dmns[i].left) && !(chck[i]))){
+          //transition
+            chain.push_back(dmns[i]);
+            chck[i] = true;
+            if(FormChain(dmns))
+                return true;
+            chain.pop_back();
+            chck[i] = false;
+        }
+    }
+    return false;
+}
 
 int main(){
-        vector<dominoT> v(5);
-    v[2] = {6,1};
-    v[1] = {1, 4};
-    v[0] = {2, 6};
-    v[3] = {4, 4};
-    v[4] = {4, 3};
+    vector<domino> v(6);
+    v[0] = {1, 3};
+    v[1] = {3, 5};
+    v[2] = {3, 7};
+    v[3] = {2, 3};
+    v[4] = {4, 2};
+    v[5] = {5, 4};
 
-    if(FormsDominoChain(v))
-        cout << "yes" << endl;
-    else if(!FormsDominoChain(v))
-        cout << "No" << endl;
-
-    vector<dominoT> v2(4);
-    v[2] = {6,1};
-   // v[1] = {1, 4};
-    v[0] = {2, 6};
-    v[3] = {4, 4};
-    v[1] = {4, 3};
-
-    if(FormsDominoChain(v2))
-        cout << "yes" << endl;
-    else if(!FormsDominoChain(v2))
-        cout << "No" << endl;
-}
-
-
-bool canAppend(dominoT currDominoes, dominoT leftDomino , dominoT rightDomino){
-    return (currDominoes.leftDots == rightDomino.rightDots || currDominoes.rightDots == leftDomino.leftDots);
-}
-
-bool FormsDominoChain(vector<dominoT> & dominoes){
-    static deque<dominoT> ans;
-
-    if(ans.size() == 0){
-        ans.push_front(dominoes[0]);
-        dominoes.erase(dominoes.begin());
-        return FormsDominoChain(dominoes);
-    }
-    else if(dominoes.size() == 0){
-        for(dominoT d: ans){
-            cout  << d.leftDots << " | " <<d.rightDots<< "   ";
+    cout << FormChain(v) << endl;
+    if(FormChain(v)){
+        for(domino d: chain){
+            cout << d.left << " | " << d.right << "  " ;
         }
-        return true;
-    }
-    else {
-        for(int i = 0 ; i < dominoes.size() ; i++){
-            if(canAppend(dominoes[i], ans[0], ans[ans.size()-1])){
-                if(dominoes[i].leftDots == ans[ans.size()-1].rightDots){
-                    ans.push_back(dominoes[i]);
-                    dominoes.erase(dominoes.begin()+i);
-                    return FormsDominoChain(dominoes);
-                }
-                else{
-                    ans.push_front(dominoes[i]);
-                    dominoes.erase(dominoes.begin()+i);
-                    return FormsDominoChain(dominoes);
-                }
-            }
-        }
-    }
-    return false ;
 
+    }
 }
-
-
-
-
-
